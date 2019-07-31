4d3a879cf12cd5015cd3898085bc30b954b59791
yuan12395 <2250517266@qq.com>
2019-07-31 15:59:51 +0800

    change three

--- password_retry.py
+++ password_retry.py
@@ -8,10 +8,14 @@
 password = 'a123456'
 i = 3  #剩余机会
 while i > 0:  #True/or当i>0,最后if就不用了
+	i -= 1
 	pwd = input('请输入密码:')
 	if pwd == password:
 		print('登录成功！')
 		break #跳出循环
 	else:
-		i -= 1
-		print('密码错误，还有', i ,'次机会')
+		print('密码错误!')
+		if i > 0:
+			print('还有', i ,'次机会')
+		else:
+			print('没机会尝试了，锁住账号了！')
